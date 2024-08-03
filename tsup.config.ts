5822b1ed76d09b921d9b5ef755aa60d51468c9dc
zhuddan <yobaci@163.com>
2024-08-04 01:36:38 +0800

    1

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default ({ watch }) => (
   defineConfig({
     entry: {
-      outfile: 'src/test.ts',
+      outfile: 'src/index.ts',
     },
     format: 'cjs',
     platform: 'node',
