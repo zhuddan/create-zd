a7b2affa34753fcf18a4175ee2921e56b2d06642
zhuddan <yobaci@163.com>
2024-08-07 11:58:44 +0800

    fix

--- src/download.ts
+++ src/download.ts
@@ -6,11 +6,12 @@ import { moveFiles } from './file'
 export function downloadTemplate(templateName: string, targetName: string) {
   templateName = `template-${templateName}`
   return new Promise((resolve, reject) => {
-    const url = `https://codeload.github.com/zhuddan/${templateName}/zip/refs/heads/master`
-    https.get(url, (res) => {
+    const url = `https://codeload.github.com/zhuddan/${templateName}/zip/refs/heads/master?t=${Date.now()}`
+    const _req = https.get(url, (res) => {
       if (res.statusCode !== 200) {
         return reject(new Error(`Request Failed. Status Code: ${res.statusCode}`))
       }
+
       const downloadPath = `${templateName}-${Date.now()}-${Math.random()}`
       res
         .pipe(unzipper.Extract({ path: `./${downloadPath}` }))
@@ -26,5 +27,10 @@ export function downloadTemplate(templateName: string, targetName: string) {
         })
         .on('error', reject)
     }).on('error', reject)
+
+    _req.setTimeout(30000, () => { // 设置超时时间为30秒
+      _req.destroy() // 终止请求
+      reject(new Error('Request timed out'))
+    })
   })
 }
