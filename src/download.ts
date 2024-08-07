994bfd58cf45eca37dcea0d87c956ad063180b18
zhuddan <yobaci@163.com>
2024-08-07 12:25:58 +0800

    build

--- src/download.ts
+++ src/download.ts
@@ -7,12 +7,10 @@ export function downloadTemplate(templateName: string, targetName: string) {
   templateName = `template-${templateName}`
   return new Promise((resolve, reject) => {
     const url = `https://codeload.github.com/zhuddan/${templateName}/zip/refs/heads/master?t=${Date.now()}`
-    console.log(url)
     const _req = https.get(url, (res) => {
       if (res.statusCode !== 200) {
         return reject(new Error(`Request Failed. Status Code: ${res.statusCode}`))
       }
-
       const downloadPath = `${templateName}-${Date.now()}-${Math.random()}`
       const zipFilePath = `${templateName}-${Date.now()}-${Math.random()}.zip`
       const file = fs.createWriteStream(zipFilePath)
@@ -25,22 +23,7 @@ export function downloadTemplate(templateName: string, targetName: string) {
         await deleteFileOrDir(downloadPath)
         resolve('')
       })
-
-      // res
-      //   .pipe(unzipper.Extract({ path: `./${downloadPath}` }))
-      //   .on('close', async () => {
-      //     try {
-      //       // await moveFiles(`./${downloadPath}/${templateName}-master`, `./${targetName}`)
-      //       // fs.rmdirSync(downloadPath)
-      //       resolve('')
-      //     }
-      //     catch (error) {
-      //       reject(error)
-      //     }
-      //   })
-      // .on('error', reject)
     }).on('error', reject)
-
     _req.setTimeout(30000, () => { // 设置超时时间为30秒
       _req.destroy() // 终止请求
       reject(new Error('Request timed out'))
