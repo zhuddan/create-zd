246785d61827aae1ddc6d328cec0a8101bde61fb
zhuddan <yobaci@163.com>
2024-08-07 12:25:19 +0800

    fix

--- src/download.ts
+++ src/download.ts
@@ -1,31 +1,44 @@
 import https from 'node:https'
 import fs from 'node:fs'
 import unzipper from 'unzipper'
-import { moveFiles } from './file'
+import { deleteFileOrDir, moveFiles } from './file'
 
 export function downloadTemplate(templateName: string, targetName: string) {
   templateName = `template-${templateName}`
   return new Promise((resolve, reject) => {
     const url = `https://codeload.github.com/zhuddan/${templateName}/zip/refs/heads/master?t=${Date.now()}`
+    console.log(url)
     const _req = https.get(url, (res) => {
       if (res.statusCode !== 200) {
         return reject(new Error(`Request Failed. Status Code: ${res.statusCode}`))
       }
 
       const downloadPath = `${templateName}-${Date.now()}-${Math.random()}`
-      res
-        .pipe(unzipper.Extract({ path: `./${downloadPath}` }))
-        .on('close', async () => {
-          try {
-            await moveFiles(`./${downloadPath}/${templateName}-master`, `./${targetName}`)
-            fs.rmdirSync(downloadPath)
-            resolve('')
-          }
-          catch (error) {
-            reject(error)
-          }
-        })
-        .on('error', reject)
+      const zipFilePath = `${templateName}-${Date.now()}-${Math.random()}.zip`
+      const file = fs.createWriteStream(zipFilePath)
+      res.pipe(file)
+      res.on('close', async () => {
+        const directory = await unzipper.Open.file(zipFilePath)
+        await directory.extract({ path: `./${downloadPath}` })
+        await moveFiles(`./${downloadPath}/${templateName}-master`, `./${targetName}`)
+        await deleteFileOrDir(zipFilePath)
+        await deleteFileOrDir(downloadPath)
+        resolve('')
+      })
+
+      // res
+      //   .pipe(unzipper.Extract({ path: `./${downloadPath}` }))
+      //   .on('close', async () => {
+      //     try {
+      //       // await moveFiles(`./${downloadPath}/${templateName}-master`, `./${targetName}`)
+      //       // fs.rmdirSync(downloadPath)
+      //       resolve('')
+      //     }
+      //     catch (error) {
+      //       reject(error)
+      //     }
+      //   })
+      // .on('error', reject)
     }).on('error', reject)
 
     _req.setTimeout(30000, () => { // 设置超时时间为30秒
